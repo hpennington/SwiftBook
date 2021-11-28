35bdf4ca57d90c0fd03b4e51d3fcd0c9059b540e
Hayden Pennington <haydenpennington@icloud.com>
2021-11-27 21:27:21 -0600

    Start laying the groundwork

--- Sources/SwiftBook/SwiftBook.swift
+++ Sources/SwiftBook/SwiftBook.swift
@@ -2,17 +2,47 @@ import SwiftUI
 
 @available(iOS 13, macOS 10.15, *)
 public struct SwiftBook: View {
-    let components: [AnyView]
+    let docs: [Any]
+    let titles: [String]
+    @State var components: [AnyView]
     
-    public init(components: [AnyView]) {
-        self.components = components
+    public init?(docs: [Any]) {
+        self.docs = docs
+        self.titles = docs.compactMap({doc in
+            (doc as! SwiftBookDoc).title
+        })
+        
+        if self.titles.count > 0 {
+            self.components = (docs[0] as! SwiftBookDoc).stories
+        } else {
+            return nil
+        }
+        
     }
     
     public var body: some View {
         ScrollView {
-            ForEach(0..<components.count) { index in
-                components[index]
+            HStack {
+                VStack{
+                    ForEach(0..<titles.count) { index in
+                        Text(titles[index])
+                            .onTapGesture {
+                                components = (docs[1] as! SwiftBookDoc).stories
+                            }
+                    }
+                }
+                VStack {
+                    ForEach(0..<components.count, id: \.self) { index in
+                        components[index]
+                    }
+                }
             }
         }
     }
+}
+
+@available(iOS 13, macOS 10.15, *)
+public protocol SwiftBookDoc {
+    var title: String { get }
+    var stories: [AnyView] { get }
 }
