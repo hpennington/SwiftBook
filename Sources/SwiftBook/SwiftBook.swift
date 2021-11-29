5953168c39265a0c922566970361d9c472866ac2
Hayden Pennington <haydenpennington@icloud.com>
2021-11-28 19:25:57 -0600

    Make options horizontal

--- Sources/SwiftBook/SwiftBook.swift
+++ Sources/SwiftBook/SwiftBook.swift
@@ -35,26 +35,43 @@ public struct SwiftBook: View {
                     }
                 }
                 .frame(maxWidth: 200)
-                ScrollView {
-                    VStack {
-                        ForEach(0..<components.count, id: \.self) { index in
-                            components[index]
-                            if controls[selectedIndex].count > index {
-                                
-                                ForEach(0..<controls[selectedIndex][index].count, id: \.self) { controlIndex in
-                                    controls[selectedIndex][index][controlIndex]
-                                }
-                            }
-                        }
-                    }.frame(minWidth: 800, maxWidth: .infinity, maxHeight: .infinity)
-
-                }
+                SwiftBookCanvas(components: $components, controls: controls, selectedIndex: selectedIndex)
             }
             .frame(minWidth: 1100, minHeight: 700)
             .background(colorScheme == .dark ? Color(red: 0.1, green: 0.1, blue: 0.1) : Color(red: 0.95, green: 0.95, blue: 0.95))
             .onAppear(perform: {
                 self.components = (docs[0] as! SwiftBookDoc).stories
             })
+        }
+    }
+}
+
+@available(iOS 13, macOS 10.15, *)
+private struct SwiftBookCanvas: View {
+    @Binding var components: [AnyView]
+    let controls: [[[AnyView]]]
+    var selectedIndex: Int
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<components.count, id: \.self) { index in
+                    components[index]
+                    HStack {
+                        if controls[selectedIndex].count > index {
+                     
+                            ForEach(0..<controls[selectedIndex][index].count, id: \.self) { controlIndex in
+                                
+                                controls[selectedIndex][index][controlIndex]
+                                
+                            }
+                     
+                        }
+                    }
+                    
+                }
+            }.frame(minWidth: 800, maxWidth: .infinity, maxHeight: .infinity)
+
         }
     }
 }
@@ -89,14 +106,16 @@ public struct SwiftBookControlColor: View {
 @available(iOS 13, macOS 10.15, *)
 public struct SwiftBookControlToggle: View {
     @Binding public var active: Bool
+    let title: String
     
-    public init(active: Binding<Bool>) {
+    public init(active: Binding<Bool>, title: String) {
         self._active = active
+        self.title = title
     }
     
     public var body: some View {
         Toggle(isOn: $active) {
-            Text("test")
+            Text(title)
         }
     }
 }
