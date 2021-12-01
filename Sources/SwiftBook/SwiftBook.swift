d377256fe4dd6673092a794ecde1914b76770121
Hayden Pennington <haydenpennington@icloud.com>
2021-12-01 00:34:17 -0600

    New SwiftBook struct

--- Sources/SwiftBook/SwiftBook.swift
+++ Sources/SwiftBook/SwiftBook.swift
@@ -15,20 +15,9 @@ let argsTableWidth: CGFloat = 400
 
 @available(iOS 13, macOS 10.15, *)
 extension Color {
-    static let offWhite = Color(red: 0.95, green: 0.95, blue: 0.95)
-    static let offBlack = Color(red: 0.05, green: 0.05, blue: 0.05)
-    static let darkBackground = Color(red: 0.1, green: 0.1, blue: 0.1)
-}
-
-/// SwiftBookDoc
-///
-/// SwiftBook has the concepts of "docs" that contain the "stories".
-/// Create a doc file and conform to SwiftBookDoc.
-/// Fill the stories property with Views containing your components.
-@available(iOS 13, macOS 10.15, *)
-public protocol SwiftBookDoc: View {
-    var argsTable: [SwiftBookArgRow] { get }
-    var controls: [[AnyView]] { get }
+    public static let offWhite = Color(red: 0.95, green: 0.95, blue: 0.95)
+    public static let offBlack = Color(red: 0.05, green: 0.05, blue: 0.05)
+    public static let darkBackground = Color(red: 0.1, green: 0.1, blue: 0.1)
 }
 
 @available(iOS 13, macOS 10.15, *)
@@ -73,75 +62,39 @@ public struct SwiftBook<Content: View>: View {
                     self.content
                         .frame(minWidth: maxCanvasWidth - navigationWidth, maxWidth: .infinity, maxHeight: .infinity)
                     Spacer(minLength: 100)
-                }
-                
-//                   SwiftBookCanvas(title: doc.title, description: doc.description, stories: $stories, controls: doc.controls, argsTable: doc.argsTable, selectedIndex: selectedIndex)
-                       .background(colorScheme == .dark ? Color.darkBackground : Color.offWhite)
+                }.background(colorScheme == .dark ? Color.darkBackground : Color.offWhite)
                }
            }
-//            self.content
-//                .frame(width: 1000, height: 800)
         }
     }
 }
-//public struct SwiftBook<Content> where Content : View {
-//    @Environment(\.colorScheme) var colorScheme
-//
-//    let content: Content
-//    let titles: [String]
-//
-//    @State var stories: [AnyView] = []
-//    @State var selectedIndex = 0
-//
-//    public init(@ViewBuilder content: () -> Content) {
-//        self.content = content()
-//        self.titles = []
-////        self.titles = docs.map { $0.title }
-//    }
-//
-//    let padding: CGFloat = 15
-//    let cornerRadius: CGFloat = 10
-//
-//    public var body: some View {
-//        VStack {
-//            content
-//        }
-//
-////        if self.docs.count > selectedIndex {
-////            let doc = self.docs[selectedIndex]
-////            doc
-////            HStack {
-////                VStack(alignment: .center) {
-////                    List(0..<titles.count) { index in
-////                        Text(titles[index])
-////                            .padding(padding)
-////                            .frame(width: navigationWidth - (padding * 2), alignment: .leading)
-////                            .foregroundColor(selectedIndex == index ? .blue : .primary)
-////                            .background(colorScheme == .dark ? Color(NSColor.underPageBackgroundColor) : Color.offWhite)
-////                            .cornerRadius(cornerRadius)
-////                            .onTapGesture {
-////                                selectedIndex = index
-////                                let doc = docs[index]
-////                                stories = doc.stories
-////                            }
-////                    }
-////
-////                }
-////
-////                .frame(maxWidth: navigationWidth)
-////                VStack {
-////                    SwiftBookCanvas(title: doc.title, description: doc.description, stories: $stories, controls: doc.controls, argsTable: doc.argsTable, selectedIndex: selectedIndex)
-////                        .background(colorScheme == .dark ? Color.darkBackground : Color.offWhite)
-////                }
-////            }
-////            .frame(minWidth: windowMinWidth, minHeight: windowMinHeight)
-////            .onAppear(perform: {
-////                self.stories = doc
-////            })
-//
-////        }
-//    }
-//}
+
+@available(iOS 13, macOS 10.15, *)
+public struct ArgsTable: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    let argsTable: [SwiftBookArgRow]
+    
+    public init(argsTable: [SwiftBookArgRow]) {
+        self.argsTable = argsTable
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Text("Arguments")
+                .padding()
+                .font(.headline)
+            ForEach(0..<argsTable.count, id: \.self) { argsIndex in
+                argsTable[argsIndex]
+                    .frame(width: 400, alignment: .leading)
+                    .padding()
+                    .background(colorScheme == .dark ? Color.offBlack : .white)
+                    .foregroundColor(Color.primary)
+                    .cornerRadius(10.0)
+            }
+        }
+    }
+}
 
 @available(iOS 13, macOS 10.15, *)
 public struct SwiftBookComponent<C: View> : View {
@@ -156,80 +109,6 @@ public struct SwiftBookComponent<C: View> : View {
         .frame(maxWidth: maxCanvasWidth, alignment: .center)
         .padding()
   }
-}
-
-@available(iOS 13, macOS 10.15, *)
-private struct SwiftBookCanvas: View {
-    let title: String
-    let description: String
-    @Binding var stories: [AnyView]
-    let controls: [[AnyView]]
-    let argsTable: [SwiftBookArgRow]
-    var selectedIndex: Int
-    
-    var body: some View {
-        ScrollView(showsIndicators: false) {
-            Spacer(minLength: 100)
-            VStack {
-                Text(title)
-                    .frame(maxWidth: maxCanvasWidth, alignment: .leading)
-                    .font(.system(size: 44))
-                    .padding()
-                Text(description)
-                    .frame(maxWidth: maxCanvasWidth, alignment: .leading)
-                    .font(.system(size: 18))
-                    .padding()
-                ForEach(0..<stories.count, id: \.self) { index in
-                    SwiftBookCanvasInner(stories: $stories, controls: controls, argsTable: argsTable, index: index)
-                }
-            }
-            .frame(minWidth: maxCanvasWidth - navigationWidth, maxWidth: .infinity, maxHeight: .infinity)
-            Spacer(minLength: 100)
-        }
-        .id(selectedIndex)
-    }
-}
-
-@available(iOS 13, macOS 10.15, *)
-private struct SwiftBookCanvasInner: View {
-    @Environment(\.colorScheme) var colorScheme
-    @Binding var stories: [AnyView]
-    let controls: [[AnyView]]
-    let argsTable: [SwiftBookArgRow]
-    let index: Int
-    
-    var body: some View {
-        if index < stories.count {
-            stories[index]
-            HStack {
-                if controls.count > index {
-             
-                    ForEach(0..<controls[index].count, id: \.self) { controlIndex in
-
-                        controls[index][controlIndex]
-                        
-                    }
-             
-                }
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Arguments")
-                    .padding()
-                    .font(.headline)
-                ForEach(0..<argsTable.count, id: \.self) { argsIndex in
-                    argsTable[argsIndex]
-                        .frame(width: argsTableWidth, alignment: .leading)
-                        .padding()
-                        .background(colorScheme == .dark ? Color.offBlack : .white)
-                        .foregroundColor(Color.primary)
-                        .cornerRadius(10.0)
-                }
-            }
-            Spacer(minLength: 100)
-        }
-        
-    }
 }
 
 @available(iOS 13, macOS 10.15, *)
