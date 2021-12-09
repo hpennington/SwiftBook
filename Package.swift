594d80488a5a6fe110d770799f84f837aaec1997
Hayden Pennington <haydenpennington@icloud.com>
2021-12-09 00:17:17 -0600

    Remove dep

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftBook",
-            dependencies: ["SheetyColors"]),
+            dependencies: []),
         .testTarget(
             name: "SwiftBookTests",
             dependencies: ["SwiftBook"]),
