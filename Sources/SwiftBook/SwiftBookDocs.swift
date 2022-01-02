b006aae2fe60b49a481270512a486648964e78d6
Hayden Pennington <haydenpennington@icloud.com>
2022-01-02 04:02:21 -0600

    Cleanup

--- Sources/SwiftBook/SwiftBookDocs.swift
+++ Sources/SwiftBook/SwiftBookDocs.swift
@@ -11,14 +11,20 @@ struct SwiftBookDocs: View {
     let documentsTable: [(String, AnyView)]
     let selectedIndex: Int
     
+    #if os(macOS)
+    let spacer: CGFloat = 100
+    #else
+    let spacer: CGFloat = 60
+    #endif
+    
     var body: some View {
         ScrollView(showsIndicators: true) {
-            Spacer(minLength: 100)
+            Spacer(minLength: spacer)
             if self.documentsTable.count > self.selectedIndex {
                 self.documentsTable[self.selectedIndex].1
                     .frame(idealWidth: maxCanvasWidth - navigationWidth, maxWidth: .infinity, maxHeight: .infinity)
             }
-            Spacer(minLength: 100)
+            Spacer(minLength: spacer)
         }
     }
 }
